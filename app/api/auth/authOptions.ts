cc7731070f28bba0aebbb455264be5de37fbc1a9
macosta123 <victormacosta11@gmail.com>
2025-11-13 09:36:30 -0300

    fix: Adicionar tipos do NextAuth e corrigir erro de session.user

--- app/api/auth/authOptions.ts
+++ app/api/auth/authOptions.ts
@@ -96,7 +96,7 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async session({ session, token }) {
-      if (token) {
+      if (token && session.user) {
         session.user.id = token.id as string;
         session.user.name = token.name as string;
         session.user.email = token.email as string;
